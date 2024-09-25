3aba82dbd4973fbf8b9d59760ee4a36305de7f6c
Ryan Campos <ryanlgithub@gmail.com>
2024-09-24 23:07:04 -0300

    Retornando o prefixo /api/

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -25,8 +25,8 @@ const hello = require('../routes/hello');
 const verifyToken = require('../middlewares/VerificarToken');
 
 // Chamando as rotas
-app.use('/auth', authRoute);
-app.use('/user', verifyToken, userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/user', verifyToken, userRoute);
 // app.use('/dialogflow', verifyToken, dialogflowRoute);
 app.use('/hello', hello);
 
