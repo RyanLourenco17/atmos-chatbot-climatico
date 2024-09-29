80c6ae940acfe6145e2987ff7a3e5a4d57dcebb8
Ryan Campos <ryanlgithub@gmail.com>
2024-09-29 06:49:14 -0300

    Testando prefixo de /api/auth

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -32,7 +32,7 @@ const verifyToken = require('../middlewares/VerificarToken');
 
 // Chamando as rotas
 app.use('/api/auth', authRoute);
-app.use('/user', verifyToken, userRoute);
+app.use('/api/user', verifyToken, userRoute);
 // app.use('/dialogflow', verifyToken, dialogflowRoute);
 app.use('/hello', hello);
 
