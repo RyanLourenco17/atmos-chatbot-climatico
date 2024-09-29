9940e5286501aeb842e34d83f17b32357c0f3871
Ryan Campos <ryanlgithub@gmail.com>
2024-09-29 08:30:22 -0300

    Adicionando rota de requisição POST do Dialogflow

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,8 +1,18 @@
-const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const { getWeather } = require('./WeatherService');
+const mongoose = require('mongoose');
+
+// Importando Rotas
+const authRoute = require('../routes/AuthRoute');
+const userRoute = require('../routes/UserRoute');
+const dialogflowRoute = require('../routes/DialogFlowRoute'); // Certifique-se de que o caminho está correto
+const hello = require('../routes/hello');
+
+// Middlewares
+const verifyToken = require('../middlewares/VerificarToken');
+
+dotenv.config();
 
 const app = express();
 
@@ -14,30 +24,19 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
 app.options('*', cors());
 
 app.use(express.json());
 app.use(express.static('public'));
-dotenv.config();
-
-// Importando Rotas
-const authRoute = require('../routes/AuthRoute');
-const userRoute = require('../routes/UserRoute');
-const dialogflowRoute = require('../routes/DialogFlowRoute');
-const hello = require('../routes/hello');
-
-// Middlewares
-const verifyToken = require('../middlewares/VerificarToken');
 
 // Chamando as rotas
 app.use('/api/auth', authRoute);
 app.use('/api/user', verifyToken, userRoute);
-// app.use('/dialogflow', verifyToken, dialogflowRoute);
+app.use('/api/dialogflow', verifyToken, dialogflowRoute);
 app.use('/hello', hello);
 
 // Configurando o servidor e a porta do Express.js
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const DB_NAME = 'ATMOS';
@@ -54,17 +53,4 @@ mongoose
 
 app.listen(port, () => {
   console.log(`O backend está rodando na porta ${port}`);
-});
-
-
-app.get("/", (req, res) => {
-  res.json({ message: "Rota teste." });
-});
-
-app.get("/api/auth/register", (req, res) => {
-  res.json({message: "Rota de cadastro"});
-});
-
-app.get("/api/auth/login", (req, res) => {
-  res.json({message: "Rota de login"});
 });
--- back-end/routes/DialogFlowRoute.js
+++ back-end/routes/DialogFlowRoute.js
@@ -1,77 +1,69 @@
-// const express = require("express");
-// const app = express();
-// const bodyParser = require("body-parser");
-// const OpenWeatherMapHelper = require("openweathermap-node");
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser");
+const OpenWeatherMapHelper = require("openweathermap-node");
+const verifyToken = require('../middlewares/VerificarToken')
 
-// const helper = new OpenWeatherMapHelper({
-//   APPID: 'f6145973562b5173e9accc6e640816a5',
-//   units: "metric"
-// });
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static("public"));
-
-// app.get("/", function(request, response) {
-//   response.sendFile(__dirname + "/views/index.html");
-// });
-
-// app.post("/Dialogflow", function(request, response) {
-//   var intentName = request.body.queryResult.intent.displayName;
-
-//   switch(intentName) {
-//     case "Teste":
-//       response.json({ "fulfillmentText" : "Este teste funciona" });
-//       break;
-
-//     case "Temperatura":
-//       var cidade = request.body.queryResult.parameters['Cidade'];
-//       helper.getCurrentWeatherByCityName(cidade, (err, currentWeather) => {
-//         if(err){
-//           console.log(err);
-//           response.json({ "fulfillmentText": "Desculpe, houve um erro ao buscar os dados do clima." });
-//         }
-//         else {
-//           // Extraindo os dados do OpenWeather
-//           var temperaturaAtual = parseInt(currentWeather.main.temp);
-//           var tempMax = parseInt(currentWeather.main.temp_max);
-//           var tempMin = parseInt(currentWeather.main.temp_min);
-//           var umidade = parseInt(currentWeather.main.humidity);
-//           var velocidadeVento = parseInt(currentWeather.wind.speed);
-//           var pressao = currentWeather.main.pressure;
-//           var descricaoClima = currentWeather.weather[0].description;
-//           var visibilidade = currentWeather.visibility;
-//           var nascerDoSol = new Date(currentWeather.sys.sunrise * 1000).toLocaleTimeString();
-//           var porDoSol = new Date(currentWeather.sys.sunset * 1000).toLocaleTimeString();
-
-//           // Convertendo o Unix timestamp para data e hora local
-//           var dataHora = new Date(currentWeather.dt * 1000).toLocaleString();
-
-//           response.json({
-//             "fulfillmentText":
-//               "Cidade: " + currentWeather.name + "\n" +
-//               "Data e Hora: " + dataHora + "\n" +
-//               "Temperatura Atual: " + temperaturaAtual + "º" + "\n" +
-//               "Temperatura Máxima: " + tempMax + "º" + "\n" +
-//               "Temperatura Mínima: " + tempMin + "º" + "\n" +
-//               "Umidade: " + umidade + "%" + "\n" +
-//               "Velocidade do vento: " + velocidadeVento + "km/h" + "\n" +
-//               "Pressão Atmosférica: " + pressao + " hPa" + "\n" +
-//               "Descrição do clima: " + descricaoClima + "\n" +
-//               "Visibilidade: " + visibilidade + " metros" + "\n" +
-//               "Nascer do sol: " + nascerDoSol + "\n" +
-//               "Pôr do sol: " + porDoSol
-//           });
-//         }
-//       });
-//       break;
-//       default:
-//       response.json({ "fulfillmentText": "Desculpe, não entendi sua solicitação." });
-//   }
-// });
-
-// const listener = app.listen(process.env.PORT || 3000, function() {
-//   console.log("Your app is listening on port " + listener.address().port);
-// });
+const helper = new OpenWeatherMapHelper({
+  APPID: process.env.OPENWEATHER_API_KEY,
+  units: "metric"
+});
 
 
+// Ryan
+router.post("/Dialogflow", verifyToken, async (req, res) => {
+  var intentName = request.body.queryResult.intent.displayName;
+
+  switch(intentName) {
+    case "Teste":
+      response.json({ "fulfillmentText": "Este teste funciona" });
+      break;
+
+    case "Temperatura":
+      var cidade = request.body.queryResult.parameters['Cidade'];
+      helper.getCurrentWeatherByCityName(cidade, (err, currentWeather) => {
+        if(err){
+          console.log(err);
+          response.json({ "fulfillmentText": "Desculpe, houve um erro ao buscar os dados do clima." });
+        }
+        else {
+          // Extraindo os dados do OpenWeather
+          var temperaturaAtual = parseInt(currentWeather.main.temp);
+          var tempMax = parseInt(currentWeather.main.temp_max);
+          var tempMin = parseInt(currentWeather.main.temp_min);
+          var umidade = parseInt(currentWeather.main.humidity);
+          var velocidadeVento = parseInt(currentWeather.wind.speed);
+          var pressao = currentWeather.main.pressure;
+          var descricaoClima = currentWeather.weather[0].description;
+          var visibilidade = currentWeather.visibility;
+          var nascerDoSol = new Date(currentWeather.sys.sunrise * 1000).toLocaleTimeString();
+          var porDoSol = new Date(currentWeather.sys.sunset * 1000).toLocaleTimeString();
+
+          // Convertendo o Unix timestamp para data e hora local
+          var dataHora = new Date(currentWeather.dt * 1000).toLocaleString();
+
+          response.json({
+            "fulfillmentText":
+              "Cidade: " + currentWeather.name + "\n" +
+              "Data e Hora: " + dataHora + "\n" +
+              "Temperatura Atual: " + temperaturaAtual + "º" + "\n" +
+              "Temperatura Máxima: " + tempMax + "º" + "\n" +
+              "Temperatura Mínima: " + tempMin + "º" + "\n" +
+              "Umidade: " + umidade + "%" + "\n" +
+              "Velocidade do vento: " + velocidadeVento + "km/h" + "\n" +
+              "Pressão Atmosférica: " + pressao + " hPa" + "\n" +
+              "Descrição do clima: " + descricaoClima + "\n" +
+              "Visibilidade: " + visibilidade + " metros" + "\n" +
+              "Nascer do sol: " + nascerDoSol + "\n" +
+              "Pôr do sol: " + porDoSol
+          });
+        }
+      });
+      break;
+
+    default:
+      response.json({ "fulfillmentText": "Desculpe, não entendi sua solicitação." });
+  }
+});
+
+module.exports = router;
