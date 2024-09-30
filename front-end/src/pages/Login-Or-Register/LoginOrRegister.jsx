7d392bf792b1ea6a0bff24d9b80b27e5db072f98
Ryan Campos <ryanlgithub@gmail.com>
2024-09-29 21:54:36 -0300

    Adicionando componente de Loading no formulário de cadastro e login

--- front-end/src/pages/Login-Or-Register/LoginOrRegister.jsx
+++ front-end/src/pages/Login-Or-Register/LoginOrRegister.jsx
@@ -3,6 +3,7 @@ import { Tabs, Tab, Form, Button } from 'react-bootstrap';
 import { validateEmail } from '../../validations/validateEmail';
 import { validateRequired } from '../../validations/validateRequired';
 import { useNavigate } from 'react-router-dom';
+import Loading from '../../components/LoadingWave/Loading'
 import './LoginOrRegister.css';
 
 const LoginOrRegister = () => {
@@ -10,6 +11,7 @@ const LoginOrRegister = () => {
   const [key, setKey] = useState('login');
   const [errors, setErrors] = useState({});
   const [message, setMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const [loginForm, setLoginForm] = useState({
     email: '',
@@ -38,6 +40,7 @@ const LoginOrRegister = () => {
 
   const handleLoginSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     const newErrors = {};
 
     const emailError = validateEmail(loginForm.email);
@@ -75,10 +78,13 @@ const LoginOrRegister = () => {
         console.error('Erro ao fazer login:', error);
       }
     }
+
+    setIsLoading(false);
   };
 
   const handleRegisterSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     const newErrors = {};
 
     const nameError = validateRequired(registerForm.name, 'Nome');
@@ -119,99 +125,109 @@ const LoginOrRegister = () => {
         console.error('Erro ao fazer cadastro:', error);
       }
     }
+
+    setIsLoading(false);
   };
 
   return (
-    <div className="login-register-container">
-      <Tabs
-        id="login-register-tabs"
-        activeKey={key}
-        variant={'pill'}
-        onSelect={(k) => setKey(k)}
-        className="mb-5 custom-tabs justify-content-center"
-      >
-        {/* Login */}
-        <Tab eventKey="login" title="ENTRAR" tabClassName="custom-tab">
-          <Form className="custom-form" onSubmit={handleLoginSubmit}>
-            <Form.Floating className="mb-3">
-              <Form.Control
-                id="formEmailLogin"
-                type="email"
-                className={`custom-input ${errors.email ? 'is-invalid' : ''}`}
-                placeholder="Digite seu email"
-                value={loginForm.email}
-                onChange={(e) => setLoginForm({ ...loginForm, email: e.target.value })}
-              />
-              <Form.Label className='label' htmlFor="formEmailLogin">Email</Form.Label>
-              {errors.email && <div className="invalid-feedback">{errors.email}</div>}
-            </Form.Floating>
+    <>
+      {isLoading ? (
+        <div className="loading">
+          <Loading/>
+        </div>
+      ) : (
+        <div className="login-register-container">
+          <Tabs
+            id="login-register-tabs"
+            activeKey={key}
+            variant={'pill'}
+            onSelect={(k) => setKey(k)}
+            className="mb-5 custom-tabs justify-content-center"
+          >
+            {/* Login */}
+            <Tab eventKey="login" title="ENTRAR" tabClassName="custom-tab">
+              <Form className="custom-form" onSubmit={handleLoginSubmit}>
+                <Form.Floating className="mb-3">
+                  <Form.Control
+                    id="formEmailLogin"
+                    type="email"
+                    className={`custom-input ${errors.email ? 'is-invalid' : ''}`}
+                    placeholder="Digite seu email"
+                    value={loginForm.email}
+                    onChange={(e) => setLoginForm({ ...loginForm, email: e.target.value })}
+                  />
+                  <Form.Label className='label' htmlFor="formEmailLogin">Email</Form.Label>
+                  {errors.email && <div className="invalid-feedback">{errors.email}</div>}
+                </Form.Floating>
 
-            <Form.Floating className="mb-3">
-              <Form.Control
-                id="formPasswordLogin"
-                type="password"
-                className={`custom-input ${errors.password ? 'is-invalid' : ''}`}
-                placeholder="Digite sua senha"
-                value={loginForm.password}
-                onChange={(e) => setLoginForm({ ...loginForm, password: e.target.value })}
-              />
-              <Form.Label className='label' htmlFor="formPasswordLogin">Senha</Form.Label>
-              {errors.password && <div className="invalid-feedback">{errors.password}</div>}
-            </Form.Floating>
+                <Form.Floating className="mb-3">
+                  <Form.Control
+                    id="formPasswordLogin"
+                    type="password"
+                    className={`custom-input ${errors.password ? 'is-invalid' : ''}`}
+                    placeholder="Digite sua senha"
+                    value={loginForm.password}
+                    onChange={(e) => setLoginForm({ ...loginForm, password: e.target.value })}
+                  />
+                  <Form.Label className='label' htmlFor="formPasswordLogin">Senha</Form.Label>
+                  {errors.password && <div className="invalid-feedback">{errors.password}</div>}
+                </Form.Floating>
 
-            <Button type="submit" className="custom-button">Entrar</Button>
-          </Form>
-        </Tab>
+                <Button type="submit" className="custom-button">Entrar</Button>
+              </Form>
+            </Tab>
 
-        {/* Cadastro */}
-        <Tab eventKey="register" title="CADASTRAR" tabClassName="custom-tab">
-          <Form className="custom-form" onSubmit={handleRegisterSubmit}>
-            <Form.Floating className="mb-3">
-              <Form.Control
-                id="formNameRegister"
-                type="text"
-                className={`custom-input ${errors.name ? 'is-invalid' : ''}`}
-                placeholder="Digite seu nome"
-                value={registerForm.name}
-                onChange={(e) => setRegisterForm({ ...registerForm, name: e.target.value })}
-              />
-              <Form.Label className='label' htmlFor="formNameRegister">Nome</Form.Label>
-              {errors.name && <div className="invalid-feedback">{errors.name}</div>}
-            </Form.Floating>
+            {/* Cadastro */}
+            <Tab eventKey="register" title="CADASTRAR" tabClassName="custom-tab">
+              <Form className="custom-form" onSubmit={handleRegisterSubmit}>
+                <Form.Floating className="mb-3">
+                  <Form.Control
+                    id="formNameRegister"
+                    type="text"
+                    className={`custom-input ${errors.name ? 'is-invalid' : ''}`}
+                    placeholder="Digite seu nome"
+                    value={registerForm.name}
+                    onChange={(e) => setRegisterForm({ ...registerForm, name: e.target.value })}
+                  />
+                  <Form.Label className='label' htmlFor="formNameRegister">Nome</Form.Label>
+                  {errors.name && <div className="invalid-feedback">{errors.name}</div>}
+                </Form.Floating>
 
-            <Form.Floating className="mb-3">
-              <Form.Control
-                id="formEmailRegister"
-                type="email"
-                className={`custom-input ${errors.email ? 'is-invalid' : ''}`}
-                placeholder="Digite seu email"
-                value={registerForm.email}
-                onChange={(e) => setRegisterForm({ ...registerForm, email: e.target.value })}
-              />
-              <Form.Label className='label' htmlFor="formEmailRegister">Email</Form.Label>
-              {errors.email && <div className="invalid-feedback">{errors.email}</div>}
-            </Form.Floating>
+                <Form.Floating className="mb-3">
+                  <Form.Control
+                    id="formEmailRegister"
+                    type="email"
+                    className={`custom-input ${errors.email ? 'is-invalid' : ''}`}
+                    placeholder="Digite seu email"
+                    value={registerForm.email}
+                    onChange={(e) => setRegisterForm({ ...registerForm, email: e.target.value })}
+                  />
+                  <Form.Label className='label' htmlFor="formEmailRegister">Email</Form.Label>
+                  {errors.email && <div className="invalid-feedback">{errors.email}</div>}
+                </Form.Floating>
 
-            <Form.Floating className="mb-3">
-              <Form.Control
-                id="formPasswordRegister"
-                type="password"
-                className={`custom-input ${errors.password ? 'is-invalid' : ''}`}
-                placeholder="Digite sua senha"
-                value={registerForm.password}
-                onChange={(e) => setRegisterForm({ ...registerForm, password: e.target.value })}
-              />
-              <Form.Label className='label' htmlFor="formPasswordRegister">Senha</Form.Label>
-              {errors.password && <div className="invalid-feedback">{errors.password}</div>}
-            </Form.Floating>
-            
-            {message && <div className="alert alert-info">{message}</div>}
+                <Form.Floating className="mb-3">
+                  <Form.Control
+                    id="formPasswordRegister"
+                    type="password"
+                    className={`custom-input ${errors.password ? 'is-invalid' : ''}`}
+                    placeholder="Digite sua senha"
+                    value={registerForm.password}
+                    onChange={(e) => setRegisterForm({ ...registerForm, password: e.target.value })}
+                  />
+                  <Form.Label className='label' htmlFor="formPasswordRegister">Senha</Form.Label>
+                  {errors.password && <div className="invalid-feedback">{errors.password}</div>}
+                </Form.Floating>
+                
+                {message && <div className="alert alert-info">{message}</div>}
 
-            <Button type="submit" className="custom-button">Cadastrar</Button>
-          </Form>
-        </Tab>
-      </Tabs>
-    </div>
+                <Button type="submit" className="custom-button">Cadastrar</Button>
+              </Form>
+            </Tab>
+          </Tabs>
+        </div>
+      )}
+    </>
   );
 };
 
