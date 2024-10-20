c697bf69652c5e6b047ea6cb6925d3f19101b132
Ryan Campos <ryanlgithub@gmail.com>
2024-10-20 18:40:01 -0300

    Corrigindo o código  para obter  as coordenadas da cidade

--- back-end/intents/poluicaoAr.js
+++ back-end/intents/poluicaoAr.js
@@ -8,39 +8,49 @@ const helper = new OpenWeatherMapHelper({
 });
 
 const poluicaoArIntent = async (cidade, newConsultation, res) => {
-  // Checa a poluição do ar usando a API OpenWeather
-  helper.getCurrentAirPollutionByCityName(cidade, async (err, pollutionData) => {
+  helper.getCurrentWeatherByCityName(cidade, async (err, currentWeather) => {
     if (err) {
       console.log(err);
-      return res.json({ "fulfillmentText": "Desculpe, houve um erro ao buscar os dados de poluição do ar." });
+      return res.json({ "fulfillmentText": "Desculpe, houve um erro ao buscar os dados da cidade." });
     } else {
-      // Extraindo informações de poluição do ar
-      const qualidadeAr = pollutionData.list[0].main.aqi; // Índice de qualidade do ar
-      const pm2_5 = pollutionData.list[0].components.pm2_5;
-      const pm10 = pollutionData.list[0].components.pm10;
-      const no2 = pollutionData.list[0].components.no2;
-      const o3 = pollutionData.list[0].components.o3;
-      const co = pollutionData.list[0].components.co;
+      const lat = currentWeather.coord.lat;
+      const lon = currentWeather.coord.lon;
 
-      const resposta =
-        "Cidade: " + cidade + "\n" +
-        "Qualidade do Ar (1-5): " + qualidadeAr + "\n" +
-        "PM2.5: " + pm2_5 + " µg/m³\n" +
-        "PM10: " + pm10 + " µg/m³\n" +
-        "NO2: " + no2 + " µg/m³\n" +
-        "O3: " + o3 + " µg/m³\n" +
-        "CO: " + co + " µg/m³";
+      // Agora, obtenha os dados de poluição do ar com as coordenadas
+      helper.getCurrentAirPollution(lat, lon, async (err, pollutionData) => {
+        if (err) {
+          console.log(err);
+          return res.json({ "fulfillmentText": "Desculpe, houve um erro ao buscar os dados de poluição do ar." });
+        } else {
+          // Extraindo informações de poluição do ar
+          const qualidadeAr = pollutionData.list[0].main.aqi;
+          const pm2_5 = pollutionData.list[0].components.pm2_5;
+          const pm10 = pollutionData.list[0].components.pm10;
+          const no2 = pollutionData.list[0].components.no2;
+          const o3 = pollutionData.list[0].components.o3;
+          const co = pollutionData.list[0].components.co;
 
-      // Cria uma nova mensagem com a resposta
-      const newMessage = new Message({ question: cidade, answer: resposta });
-      await newMessage.save();
+          const resposta =
+            "Cidade: " + cidade + "\n" +
+            "Qualidade do Ar (1-5): " + qualidadeAr + "\n" +
+            "PM2.5: " + pm2_5 + " µg/m³\n" +
+            "PM10: " + pm10 + " µg/m³\n" +
+            "NO2: " + no2 + " µg/m³\n" +
+            "O3: " + o3 + " µg/m³\n" +
+            "CO: " + co + " µg/m³";
 
-      // Adiciona a mensagem à consulta
-      newConsultation.messages.push(newMessage._id);
-      await newConsultation.save();
+          // Cria uma nova mensagem com a resposta
+          const newMessage = new Message({ question: cidade, answer: resposta });
+          await newMessage.save();
 
-      // Responde ao cliente
-      res.json({ "fulfillmentText": resposta, consultationId: newConsultation._id });
+          // Adiciona a mensagem à consulta
+          newConsultation.messages.push(newMessage._id);
+          await newConsultation.save();
+
+          // Responde ao cliente
+          res.json({ "fulfillmentText": resposta, consultationId: newConsultation._id });
+        }
+      });
     }
   });
 };
