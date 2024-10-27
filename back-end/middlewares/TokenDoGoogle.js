842ee62eaaf0f258898c75618594d9518ea296ee
Ryan Campos <ryanlgithub@gmail.com>
2024-10-27 17:39:13 -0300

    Adicionando e imporantando função de getAccessToken

---
+++ back-end/middlewares/TokenDoGoogle.js
@@ -0,0 +1,14 @@
+const { google } = require('googleapis');
+
+const getAccessToken = async () => {
+  const jwtClient = new google.auth.JWT(
+    process.env.DIALOGFLOW_CLIENT_EMAIL, // Email do serviço
+    null,
+    process.env.DIALOGFLOW_PRIVATE_KEY.replace(/\\n/g, '\n'), // Chave privada
+    ['https://www.googleapis.com/auth/cloud-platform'] // Escopos necessários
+  );
+
+  await jwtClient.authorize();
+  const accessToken = jwtClient.credentials.access_token;
+  return accessToken;
+};
