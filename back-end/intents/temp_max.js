2ada5015f0e7c2121e21cf04f2ebc55fe9bb7314
Ryan Campos <ryanlgithub@gmail.com>
2024-11-04 16:05:30 -0300

    .

--- back-end/intents/temp_max.js
+++ back-end/intents/temp_max.js
@@ -1,42 +1,42 @@
 const helper = require('../middlewares/helper');
 
 function getRandomResponse(respostas) {
-    return respostas[Math.floor(Math.random() * respostas.length)];
+  return respostas[Math.floor(Math.random() * respostas.length)];
 }
 
 module.exports = async (req, res) => {
-    const cidade = req.body.queryResult.parameters["cidade"];
-    if (!cidade) {
-        return res.json({ "fulfillmentText": "Por favor, forneça o nome do lugar para obter a previsão de temperatura máxima." });
+  const cidade = req.body.queryResult.parameters["cidade"];
+  if (!cidade) {
+    return res.json({ "fulfillmentText": "Por favor, forneça o nome do lugar para obter a previsão de temperatura máxima." });
+  }
+
+  try {
+    const currentWeather = await new Promise((resolve, reject) => {
+      helper.getCurrentWeatherByCityName(cidade, (err, currentWeather) => {
+          if (err) {
+              console.error("Erro ao buscar clima: ", err);
+              return reject(err);
+          }
+          resolve(currentWeather);
+      });
+    });
+
+    if (!currentWeather || currentWeather.cod !== 200) {
+      return res.json({ "fulfillmentText": "Não foi possível encontrar o lugar ou obter as informações climáticas." });
     }
 
-    try {
-        const currentWeather = await new Promise((resolve, reject) => {
-            helper.getCurrentWeatherByCityName(cidade, (err, currentWeather) => {
-                if (err) {
-                    console.error("Erro ao buscar clima: ", err);
-                    return reject(err);
-                }
-                resolve(currentWeather);
-            });
-        });
+    const tempMax = Math.round(currentWeather.main.temp_max);
+    const respostas = [
+      `A previsão de temperatura máxima para ${currentWeather.name} é de aproximadamente ${tempMax}°C.`,
+      `Hoje, a temperatura máxima esperada em ${currentWeather.name} é de ${tempMax}°C.`,
+      `Em ${currentWeather.name}, a temperatura pode alcançar até ${tempMax}°C hoje.`
+    ];
 
-        if (!currentWeather || currentWeather.cod !== 200) {
-            return res.json({ "fulfillmentText": "Não foi possível encontrar o lugar ou obter as informações climáticas." });
-        }
-
-        const tempMax = Math.round(currentWeather.main.temp_max);
-        const respostas = [
-            `A previsão de temperatura máxima para ${currentWeather.name} é de aproximadamente ${tempMax}°C.`,
-            `Hoje, a temperatura máxima esperada em ${currentWeather.name} é de ${tempMax}°C.`,
-            `Em ${currentWeather.name}, a temperatura pode alcançar até ${tempMax}°C hoje.`
-        ];
-
-        res.json({
-            fulfillmentText: getRandomResponse(respostas),
-        });
-    } catch (error) {
-        console.error("Erro ao buscar temperatura máxima: ", error);
-        return res.json({ "fulfillmentText": "Desculpe, não conseguimos obter os dados de temperatura máxima." });
-    }
+    res.json({
+        fulfillmentText: getRandomResponse(respostas),
+    });
+  } catch (error) {
+    console.error("Erro ao buscar temperatura máxima: ", error);
+    return res.json({ "fulfillmentText": "Desculpe, não conseguimos obter os dados de temperatura máxima." });
+  }
 };
--- back-end/intents/temp_min.js
+++ back-end/intents/temp_min.js
@@ -1,43 +1,43 @@
 const helper = require('../middlewares/helper');
 
 function getRandomResponse(respostas) {
-    return respostas[Math.floor(Math.random() * respostas.length)];
+  return respostas[Math.floor(Math.random() * respostas.length)];
 }
 
 module.exports = async (req, res) => {
-    const cidade = req.body.queryResult.parameters["cidade"];
+  const cidade = req.body.queryResult.parameters["cidade"];
 
-    if (!cidade) {
-        return res.json({ "fulfillmentText": "Por favor, forneça o nome do lugar para obter a previsão de temperatura mínima." });
+  if (!cidade) {
+    return res.json({ "fulfillmentText": "Por favor, forneça o nome do lugar para obter a previsão de temperatura mínima." });
+  }
+
+  try {
+    const currentWeather = await new Promise((resolve, reject) => {
+        helper.getCurrentWeatherByCityName(cidade, (err, currentWeather) => {
+            if (err) {
+                console.error("Erro ao buscar clima: ", err);
+                return reject(err);
+            }
+            resolve(currentWeather);
+        });
+    });
+
+    if (!currentWeather || currentWeather.cod !== 200) {
+        return res.json({ "fulfillmentText": "Não foi possível encontrar o lugar ou obter as informações climáticas." });
     }
 
-    try {
-        const currentWeather = await new Promise((resolve, reject) => {
-            helper.getCurrentWeatherByCityName(cidade, (err, currentWeather) => {
-                if (err) {
-                    console.error("Erro ao buscar clima: ", err);
-                    return reject(err);
-                }
-                resolve(currentWeather);
-            });
-        });
+    const tempMin = Math.round(currentWeather.main.temp_min);
+    const respostas = [
+        `A previsão de temperatura mínima para ${currentWeather.name} é de aproximadamente ${tempMin}°C.`,
+        `Hoje, a menor temperatura esperada em ${currentWeather.name} é de ${tempMin}°C.`,
+        `Em ${currentWeather.name}, a temperatura mínima prevista é de ${tempMin}°C.`
+    ];
 
-        if (!currentWeather || currentWeather.cod !== 200) {
-            return res.json({ "fulfillmentText": "Não foi possível encontrar o lugar ou obter as informações climáticas." });
-        }
-
-        const tempMin = Math.round(currentWeather.main.temp_min);
-        const respostas = [
-            `A previsão de temperatura mínima para ${currentWeather.name} é de aproximadamente ${tempMin}°C.`,
-            `Hoje, a menor temperatura esperada em ${currentWeather.name} é de ${tempMin}°C.`,
-            `Em ${currentWeather.name}, a temperatura mínima prevista é de ${tempMin}°C.`
-        ];
-
-        res.json({
-            fulfillmentText: getRandomResponse(respostas),
-        });
-    } catch (error) {
-        console.error("Erro ao buscar temperatura mínima: ", error);
-        return res.json({ "fulfillmentText": "Desculpe, não conseguimos obter os dados de temperatura mínima." });
-    }
+    res.json({
+        fulfillmentText: getRandomResponse(respostas),
+    });
+  } catch (error) {
+    console.error("Erro ao buscar temperatura mínima: ", error);
+    return res.json({ "fulfillmentText": "Desculpe, não conseguimos obter os dados de temperatura mínima." });
+  }
 };
