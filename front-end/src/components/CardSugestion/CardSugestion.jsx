7d50606abf26ad0fe4f3b9fcc52165298e2842d4
Ryan Campos <105888654+RyanLourenco17@users.noreply.github.com>
2024-11-11 19:50:08 -0300

    Update CardSugestion.jsx

--- front-end/src/components/CardSugestion/CardSugestion.jsx
+++ front-end/src/components/CardSugestion/CardSugestion.jsx
@@ -5,16 +5,16 @@ const CardSugestion = () => {
     <>
         <div className="grid-card">
             <div className="card-body">
-                <p className="card-title">Qual é a previsão do tempo para hoje?</p>
+                <p className="card-title">Qual é o nível de poluição em Osasco?</p>
             </div>
             <div className="card-body">
-                <p className="card-title">Qual é a velocidade do vento?</p>
+                <p className="card-title">Qual é a velocidade do vento em Santos?</p>
             </div>
             <div className="card-body">
-                <p className="card-title">Há previsão de tempestade?</p>
+                <p className="card-title">Qual a pressão atmosférica em Recife ?</p>
             </div>
             <div className="card-body">
-                <p className="card-title">Como está o tempo em São Paulo?</p>
+                <p className="card-title">Como está o clima atual em Campinas?</p>
             </div>
         </div>
     </>
